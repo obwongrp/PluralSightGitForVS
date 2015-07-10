72a0bb3b1f45e6b98092bd7b72a1395fe98a103d
Keith Bott <kbott@iec-electronics.com>
2015-07-10 15:43:40 -0400

    Initial vsc2013 Commit

---
+++ PS VSC2013 Demo/PS VSC2013 Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS_VSC2013_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
